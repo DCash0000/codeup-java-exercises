73a47fff83a2c7094976b99e89ca6485dc08203f
Dalvin Cash <cashdalvin1@gmail.com>
2020-02-12 09:01:29 -0600

    edits

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -6,48 +6,48 @@ public class ConsoleExercises {
         //Copy this code into your main method//
         System.out.printf("The value of pi is approximately %f.\n\n", pi);
 
-//
-//        //Prompt a user to enter a integer and store that value in an int variable using the nextInt method.//
-//        Scanner scanner = new Scanner(System.in);
-//        int i = 0;
-//        while(i != -1){
-//            System.out.print("enter an interger: ");
-//            i = scanner.nextInt();
-//            System.out.println("\nThe number is " + i + ".\n");
-//        }
-//
-//        //Prompt a user to enter 3 words, and store each of them in a separate variable. Then, display them back in the console, each on a newline.//
-//        System.out.print("enter 3 words: ");
-//        String word01, word02, word03;
-//        word01 = scanner.next();
-//        word02 = scanner.next();
-//        word03 = scanner.next();
-//        System.out.printf("\n the words are \"%s\", \"%s\", and \"%s\".\n", word01, word02, word03);
-//        scanner.nextLine();
-//
-//
-//        //Prompt a user to enter a sentence, then store that sentence in a String variable using the next method. Then, display that sentence back to the user.//
-//        System.out.print("\nEnter a sentence: ");
-//        scanner.useDelimiter("\n");
-//        String sentence = scanner.next();
-//        scanner.nextLine();
-//        System.out.printf("\nThe sentence is \"%s\"\n", sentence);
-//
-//        //Rewrite the above example using the nextLine method.//
-//        System.out.print("\nEnter a sentence: ");
-//        String sentence1 = scanner.nextLine();
-//        System.out.printf("\nThe sentence is \"%s\"\n", sentence1);
-//
-//
-//        //Prompt the user to enter values of length and width of a classroom at Codeup.//
-//        Scanner scan = new Scanner(System.in);
-//        System.out.print("\nEnter the Width: ");
-//        String sWidth = scan.nextLine();
-//        double width = Double.parseDouble(sWidth);
-//        System.out.print("Enter the length: ");
-//        String sLength = scan.nextLine();
-//        double length = Double.parseDouble(sLength);
-//        System.out.printf("The dimensions are %f. the perimeter is %f and the area is %f. \n", length, width, 2*(length+width), length*width);
+
+        //Prompt a user to enter a integer and store that value in an int variable using the nextInt method.//
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+        if(i != -1){
+            System.out.print("enter an interger: ");
+            i = scanner.nextInt();
+            System.out.println("The number is " + i );
+        }
+
+        //Prompt a user to enter 3 words, and store each of them in a separate variable. Then, display them back in the console, each on a newline.//
+        System.out.print("enter 3 words: ");
+        String word01, word02, word03;
+        word01 = scanner.next();
+        word02 = scanner.next();
+        word03 = scanner.next();
+        System.out.printf("\n the words are \"%s\", \"%s\", and \"%s\".\n", word01, word02, word03);
+        scanner.nextLine();
+
+
+        //Prompt a user to enter a sentence, then store that sentence in a String variable using the next method. Then, display that sentence back to the user.//
+        System.out.print("\nEnter a sentence: ");
+        scanner.useDelimiter("\n");
+        String sentence = scanner.next();
+        scanner.nextLine();
+        System.out.printf("\nThe sentence is \"%s\"\n", sentence);
+
+        //Rewrite the above example using the nextLine method.//
+        System.out.print("\nEnter a sentence: ");
+        String sentence1 = scanner.nextLine();
+        System.out.printf("\nThe sentence is \"%s\"\n", sentence1);
+
+
+        //Prompt the user to enter values of length and width of a classroom at Codeup.//
+        Scanner scan = new Scanner(System.in);
+        System.out.print("\nEnter the Width: ");
+        String sWidth = scan.nextLine();
+        double width = Double.parseDouble(sWidth);
+        System.out.print("Enter the length: ");
+        String sLength = scan.nextLine();
+        double length = Double.parseDouble(sLength);
+        System.out.printf("The dimensions are %f. the perimeter is %f and the area is %f. \n", length, width, 2*(length+width), length*width);
 
 
 //        Bonuses//
